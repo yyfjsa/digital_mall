9501caae5901e3f1acfbfd87ded3ba31318070f1
yyfjsa <149576430+yyfjsa@users.noreply.github.com>
2025-04-10 21:22:46 +0800

    Delete src/main directory

--- src/main/java/com/cow/dao/InfraFileDao.java
+++
@@ -1,9 +0,0 @@
-package com.cow.dao;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cow.entity.InfFileDO;
-
-public interface InfraFileDao extends BaseMapper<InfFileDO> {
-
-}
